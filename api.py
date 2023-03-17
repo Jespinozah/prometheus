8f38d9eb71dd0b126486ee72ddcf3da409098148
OrsoCaruzzo <cdcr_00@outlook.com>
2023-03-17 00:03:57 -0500

    Token authentication beta

---
+++ api.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify, request, make_response
+import jwt 
+import datetime
+from functools import wraps
+app= Flask(__name__)
+app.config['SECRET_KEY'] = 'thisisthesecretkey'
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args,**kwargs):
+        token=request.args.get('token') #http://127.0.0.1:5000/route?token=asdasdasdasd
+        if not token:
+            return jsonify({'message' : 'Token is missing!' }), 403
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+        except: 
+            return jsonify({'message' : 'Token is invalid!' }), 403
+        return f(*args, **kwargs)
+    return decorated
+
+@app.route('/unprotected')
+def unprotected():
+    return ''
+
+@app.route('/protected')
+@token_required
+def protected():
+    return  jsonify({'message' : 'This is only available for people with valid tokens'})
+
+@app.route('/login')
+def login():
+    auth= request.authorization
+    if auth and auth.password =='password':
+        token = jwt.encode({'user': auth.username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},app.config['SECRET_KEY'])
+        return  jsonify({'token' : token.encode().decode('UTF-8')})
+    
+    return make_response('Could not verify!', 401,{'WWW-Authenticate' : 'Basic realm = "Login Required"'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
